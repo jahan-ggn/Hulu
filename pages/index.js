ddde3051f7bede50a60d25ccf9275ee384157451
Jahan Gagan <jahan@mobiux.in>
2021-12-17 16:32:48 +0530

    Infinite scrolling added but on changing tabs it is not reflecting with the expected data

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,34 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import requests from '../utils/requests'
-import Result from '../components/Results';
+import Thumbnail from '../components/Thumbnail'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
 
 export default function Home({ results }) {
-  console.log("RESULT ===> ", results)
+  const [movies, setMovies] = useState(results);
+  const [hasMore, setHasMore] = useState(true);
+  const [pageNo, setpageNo] = useState(2);
+
+  const router = useRouter()
+  
+  const getMoreMovies = async() => {
+    const request = await fetch(
+      `https://api.themoviedb.org/3${requests[router.query.genre]?.url || requests.fetchTrendingMovies.url
+      }&page=${pageNo}`
+    )
+    console.log("URL ===> ", request.url)
+    const newMovies = await request.json();
+    console.log("RESPONSE ===> ", newMovies.results)
+    setMovies([...movies, ...newMovies.results]);
+    if(pageNo === 1000){
+      setHasMore(false)
+    }
+    setpageNo(pageNo + 1)
+  }
+
   return (
     <div>
       <Head>
@@ -16,7 +40,22 @@ export default function Home({ results }) {
       <Header />
 
       <Nav />
-      <Result results={results} />
+      <InfiniteScroll
+      dataLength={movies.length}
+      next={getMoreMovies}
+      hasMore={hasMore}
+      loader={<h4> Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }>
+        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+              {movies.map((movie) => (
+                  <Thumbnail key={movie.id} result={movie}/>
+              ))}
+        </div>
+      </InfiniteScroll>
     </div>
   )
 }
@@ -26,11 +65,11 @@ export async function getServerSideProps(context) {
 
   const request = await fetch(
     `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrendingMovies.url
-    }`
+    }&page=1`
   )
   console.log("URL ===> ", request.url)
   const response = await request.json()
-  console.log("RESPONSE ===> ", response)
+  console.log("RESPONSE ===> ", response.results)
   return {
     props: {
       results: response.results
